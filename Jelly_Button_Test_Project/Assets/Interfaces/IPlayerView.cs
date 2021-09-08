529278905d9acc7d2de73c0342142ed9ec823911
yehudakarasenty <yehudakarasenty@gmail.com>
2021-09-08 15:19:46 +0300

    Add PlayerView.cs

---
+++ Jelly_Button_Test_Project/Assets/Interfaces/IPlayerView.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface IPlayerView
+{
+    Quaternion Rotation { get; set; }
+    Vector3 Position { get; set; }
+}
---
+++ Jelly_Button_Test_Project/Assets/Scripts/PlayerView.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlayerView : MonoBehaviour, IPlayerView
+{
+    public Quaternion Rotation { get => transform.rotation; set => transform.rotation = value; }
+    public Vector3 Position { get => transform.position; set => transform.position = value; }
+}
